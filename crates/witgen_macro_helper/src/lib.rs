7f3bc408d7555a8f9748868eb984fb53020b7873
Willem Wyndham <wyndham@cs.unc.edu>
2022-04-10 19:54:33 -0400

    nit: address PR review

--- crates/witgen_macro_helper/src/lib.rs
+++ crates/witgen_macro_helper/src/lib.rs
@@ -1,3 +1,8 @@
+//! This crate provides a way to parse a whole crate as a file and then parse this into a `Wit` type.
+//! Currently this is a wrapper type for `syn` types.
+//! 
+//! 
+//! 
 #![deny(warnings)]
 use std::path::Path;
 
@@ -8,20 +13,27 @@ pub mod generator;
 mod wit;
 pub use wit::Wit;
 
-/// Parse proc_macro2 tokens into Wit
-pub fn parse_tokens(item: proc_macro2::TokenStream) -> Result<Wit> {
-    item.try_into()
+/// Convence function for
+/// ```
+/// let wit: Result<Wit> = tokens.try_into()
+/// ```
+pub fn parse_tokens(tokens: proc_macro2::TokenStream) -> Result<Wit> {
+    tokens.try_into()
 }
 
-/// Read a crate starting from a single file then parse into Wit
+/// Read a crate starting from a single file then parse into a file
 pub fn parse_crate_as_file(path: &Path) -> Result<File> {
-    if let Ok(ast) = read_full_crate_source_code(path, |_| Ok(false)) {
-        Ok(ast)
+    if let Ok(file) = read_full_crate_source_code(path, |_| Ok(false)) {
+        Ok(file)
     } else {
         bail!("Failed to parse crate source {:?}", path)
     }
 }
 
+/// Convence function for
+/// ```
+/// let wit: Wit = file.into();
+/// ```
 pub fn parse_file(file: File) -> Wit {
   file.into()
 }
